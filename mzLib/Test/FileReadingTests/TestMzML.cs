b59c3d8af8dee0538ee6ed5fe3e0363b0e63e2b0
Alex <AlexSolivais@gmail.com>
2024-06-07 13:40:58 -0500

    Diagnositic Ion XIC plots

--- mzLib/Test/FileReadingTests/TestMzML.cs
+++ mzLib/Test/FileReadingTests/TestMzML.cs
@@ -12,6 +12,9 @@ using NUnit.Framework;
 using Proteomics.AminoAcidPolymer;
 using Readers;
 using Stopwatch = System.Diagnostics.Stopwatch;
+using Plotly.NET.CSharp;
+using Plotly;
+using Plotly.NET.ImageExport;
 
 namespace Test.FileReadingTests
 {
@@ -94,6 +97,134 @@ namespace Test.FileReadingTests
 
             Assert.AreEqual(6, scans[0].MassSpectrum.XArray.Count());
             Assert.AreEqual(20, scans[1].MassSpectrum.XArray.Count());
+        }
+
+        [Test]
+        public static void ProteinCocktail()
+        {
+            string mzmlPath = @"D:\Martell\2024\5_25_New_Mutant\ProteinCocktail_2\Task1-CalibrateTask\5-25-24_Sifei_1_8ProteinNPA_Labeled-calib.mzML";
+            var reader = MsDataFileReader.GetDataFile(mzmlPath);
+            reader.LoadAllStaticData();
+            var ms2Scans = reader.GetAllScansList().Where(scan => scan.MsnOrder > 1).ToList();
+            Tolerance tolerance = new PpmTolerance(20);
+            double[] rtArray = new double[ms2Scans.Count];
+            double[] intensityArray = new double[ms2Scans.Count];
+
+            for (int i = 0; i < ms2Scans.Count; i++)
+            {
+                rtArray[i] = ms2Scans[i].RetentionTime;
+                intensityArray[i] = 0;
+                int idx240 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(240.17);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx240], 240.17)) continue;
+
+                int idx509 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(509.31);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx509], 509.31)) continue;
+
+                intensityArray[i] = ms2Scans[i].MassSpectrum.YArray[idx240] + ms2Scans[i].MassSpectrum.YArray[idx509];
+            }
+
+            Chart.Line<double, double, string>(rtArray, intensityArray)
+                .WithTraceInfo("Diagnostic Ions")
+                .WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Retention Time (min)"))
+                .WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Intensity of Diagnostic Ions"))
+                .WithSize(Width: 1000, Height: 500)
+                .Show();
+        }
+
+        [Test]
+        public static void MitochondriaNPA()
+        {
+            string mzmlPath = @"D:\Martell\2024\5_25_New_Mutant\HumanProteomeSearch\Task1-CalibrateTask\5-25-24_Sifei_3_MitochondriaNPA_Labeled-calib.mzML";
+            var reader = MsDataFileReader.GetDataFile(mzmlPath);
+            reader.LoadAllStaticData();
+            var ms2Scans = reader.GetAllScansList().Where(scan => scan.MsnOrder > 1).ToList();
+            Tolerance tolerance = new PpmTolerance(20);
+            double[] rtArray = new double[ms2Scans.Count];
+            double[] intensityArray = new double[ms2Scans.Count];
+
+            for (int i = 0; i < ms2Scans.Count; i++)
+            {
+                rtArray[i] = ms2Scans[i].RetentionTime;
+                intensityArray[i] = 0;
+                int idx240 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(240.17);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx240], 240.17)) continue;
+
+                int idx509 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(509.31);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx509], 509.31)) continue;
+
+                intensityArray[i] = ms2Scans[i].MassSpectrum.YArray[idx240] + ms2Scans[i].MassSpectrum.YArray[idx509];
+            }
+
+            Chart.Line<double, double, string>(rtArray, intensityArray)
+                .WithTraceInfo("Diagnostic Ions")
+                .WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Retention Time (min)"))
+                .WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Intensity of Diagnostic Ions"))
+                .WithSize(Width: 1000, Height: 500)
+                .Show();
+        }
+
+        [Test]
+        public static void ControlXIC()
+        {
+            string mzmlPath = @"D:\Human_Ecoli_TwoProteome_60minGradient\CalibrateSearch_4_19_24\Human_Calibrated_Files\04-12-24_Human_C18_3mm_50msec_stnd-60min_1-calib.mzML";
+            var reader = MsDataFileReader.GetDataFile(mzmlPath);
+            reader.LoadAllStaticData();
+            var ms2Scans = reader.GetAllScansList().Where(scan => scan.MsnOrder > 1).ToList();
+            Tolerance tolerance = new PpmTolerance(20);
+            double[] rtArray = new double[ms2Scans.Count];
+            double[] intensityArray = new double[ms2Scans.Count];
+
+            for (int i = 0; i < ms2Scans.Count; i++)
+            {
+                rtArray[i] = ms2Scans[i].RetentionTime;
+                intensityArray[i] = 0;
+                int idx240 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(240.17);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx240], 240.17)) continue;
+
+                int idx509 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(509.31);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx509], 509.31)) continue;
+
+                intensityArray[i] = ms2Scans[i].MassSpectrum.YArray[idx240] + ms2Scans[i].MassSpectrum.YArray[idx509];
+            }
+
+            Chart.Line<double, double, string>(rtArray, intensityArray)
+                .WithTraceInfo("Diagnostic Ions")
+                .WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Retention Time (min)"))
+                .WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Intensity of Diagnostic Ions"))
+                .WithSize(Width: 1000, Height: 500)
+                .Show();
+        }
+
+        [Test]
+        public static void CAControlXIC()
+        {
+            string mzmlPath = @"D:\Martell\2024\5_25_New_Mutant\Mario_CA_Search\Task1-CalibrateTask\5-25-24_Sifei_4_Mario_CA_Labeling-calib.mzML";
+            var reader = MsDataFileReader.GetDataFile(mzmlPath);
+            reader.LoadAllStaticData();
+            var ms2Scans = reader.GetAllScansList().Where(scan => scan.MsnOrder > 1).ToList();
+            Tolerance tolerance = new PpmTolerance(20);
+            double[] rtArray = new double[ms2Scans.Count];
+            double[] intensityArray = new double[ms2Scans.Count];
+
+            for (int i = 0; i < ms2Scans.Count; i++)
+            {
+                rtArray[i] = ms2Scans[i].RetentionTime;
+                intensityArray[i] = 0;
+                int idx240 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(240.17);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx240], 240.17)) continue;
+
+                int idx509 = ms2Scans[i].MassSpectrum.GetClosestPeakIndex(509.31);
+                if (!tolerance.Within(ms2Scans[i].MassSpectrum.XArray[idx509], 509.31)) continue;
+
+                intensityArray[i] = ms2Scans[i].MassSpectrum.YArray[idx240] + ms2Scans[i].MassSpectrum.YArray[idx509];
+            }
+
+            Chart.Line<double, double, string>(rtArray, intensityArray)
+                .WithTraceInfo("Diagnostic Ions")
+                .WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Retention Time (min)"))
+                .WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("Intensity of Diagnostic Ions"))
+                .WithSize(Width: 1000, Height: 500)
+                .Show();
         }
 
         [Test]
